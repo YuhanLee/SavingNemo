d6257c7188b570156bf2efceef725d20221a1dbf
Eric Laroche <ericlaroche2@gmail.com>
2018-10-20 14:03:32 -0400

    map state of country commitments

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Section1 from './component/Section1'; 
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Section1 from "./component/Section1";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      map: {}
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ map: window.mainMap });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <ul id='countries'></ul>
+          <ul id="countries" />
         </header>
 
-				<section class="section1">
-					<h1>section 1</h1>
-					<div>
-						<Section1>	
+        <section class="section1">
+          <h1>section 1</h1>
+          <div>
+            {console.log(this.state.map)}
+            <Section1 />
+          </div>
+        </section>
 
-						</Section1>
-					</div>
-				</section>
+        <section class="section2">
+          <h1>section 2</h1>
+          <div>
+            <Section1 />
+          </div>
+        </section>
 
-				<section class="section2">
-					<h1>section 2</h1>
-						<div>
-							<Section1>	
-
-							</Section1>
-						</div>
-				</section>
-
-				<section class="section3">
-					<h1>section 2</h1>
-						<div>
-							<Section1>	
-
-							</Section1>
-						</div>
-				</section>
+        <section class="section3">
+          <h1>section 2</h1>
+          <div>
+            <Section1 />
+          </div>
+        </section>
       </div>
     );
   }
