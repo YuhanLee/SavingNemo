ac69923d9d77decee59f50df477aad0c8d564a68
Yuhan Lee <ylee050@uottawa.ca>
2018-10-21 11:06:53 -0400

    wew

--- src/component/PartnersByOceanBasin.js
+++ src/component/PartnersByOceanBasin.js
@@ -29,7 +29,8 @@ class PartnersByOceanBasin extends Component {
 										'rgba(255, 99, 132, 1)',
 										'rgba(54, 162, 235, 1)',
 										'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)'
+										'rgba(75, 192, 192, 1)', 
+										'rgba(5,196,20, 1)'
                 ],
                 borderColor: [
                     'rgba(75, 192, 192, 1)',
@@ -38,7 +39,8 @@ class PartnersByOceanBasin extends Component {
 										'rgba(255, 99, 132, 1)',
 										'rgba(54, 162, 235, 1)',
 										'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)'
+										'rgba(75, 192, 192, 1)',
+										'rgba(5,196,20, 1)'
                 ],
                 borderWidth: 1
             }]
